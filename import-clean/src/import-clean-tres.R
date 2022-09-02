036b1708331dc352106eb789f7f938a071838f6b
ginajimrios92 <georgina.jimenez@datacivica.org>
2022-09-02 14:15:39 -0400

    otra violencia en infancia y pedir ayuda
    Agregué las preguntas de violencia física en la infancia y reacciones ante la violencia de pareja

--- import-clean/src/import-clean-tres.R
+++ import-clean/src/import-clean-tres.R
@@ -9,7 +9,6 @@ p_load(argparse, foreign, tidyverse, janitor, here, data.table)
 
 direc <-  "/Users/georginajimenez92/Documents/inputs-datos/0922_endireh/"
 
-
 ##### ENDIREH 2011 ####
 files <- list(lib1=paste0(direc, "endireh_2011/TUnidas3.dbf"),
               socio=paste0(direc, "endireh_2011/TSDEM.DBF"),
@@ -72,6 +71,7 @@ saveRDS(data, files$endireh11)
 files <- list(lib=paste0(direc, "endireh_2016/conjunto_de_datos_tb_sec_xiv_endireh_2016.csv"),
               fam_ori=paste0(direc, "endireh_2016/conjunto_de_datos_tb_sec_xi_endireh_2016.csv"),
               socio=paste0(direc, "endireh_2016/conjunto_de_datos_tsdem_endireh_2016.csv"),
+              pareja=paste0(direc, "endireh_2016/conjunto_de_datos_tb_sec_xiii_endireh_2016.csv"),
               endireh16=here("import-clean/output/endireh2016.rds"))
 
 #### Funciones para 2016 y 2021
@@ -90,14 +90,22 @@ sino <- function(x){
                  x==2 ~ "No")
 }
 
+change <- function(x) {
+  x <- ifelse(x<4, 1, 0)
+}
 
+suma <- function(x) {
+  x <- ifelse(x>0, 1, 0)
+}
+
+
+#Limpiamos
 data <- read.csv(files$lib)%>%
         clean_names()%>%
         select(id_viv:t_instrum, 
         starts_with("p14_1ab"),
           fac_viv:est_dis)
          
-
 tempo <- read.csv(files$socio)%>%
          clean_names()%>%
          select(id_viv:cod_res, sexo, edad, niv, gra,
@@ -108,7 +116,16 @@ data <- left_join(data, tempo)
 tempo <- read.csv(files$fam_ori)%>%
          clean_names()%>%
          select(id_viv:t_instrum,
-                     starts_with("p11_12_"))
+                     starts_with("p11_12_"), p11_6, p11_7)
+
+data <- left_join(data, tempo)
+
+tempo <- read.csv(files$pareja)%>%
+         clean_names()%>%
+         select(id_viv:t_instrum,
+                starts_with("p13_1_"),
+                p13_4, p13_7_1, p13_7_2,
+                fac_viv:est_dis)
 
 data <- left_join(data, tempo)
 rm(tempo)
@@ -129,6 +146,8 @@ data <- mutate_at(data, vars(p14_1ab_1:p14_1ab_15), personas)%>%
                                niv==7 ~ 4,
                                niv==8 ~ 3,
                                niv>8 ~ 5),
+                p11_6=ifelse(p11_6<3, 1, 2),
+                p11_7=ifelse(p11_7<3, 1, 2),
          escol=factor(escol, levels=c(1:5), 
                       labels=c("Sin escolaridad",
                                "Primaria",
@@ -149,9 +168,17 @@ data <- mutate_at(data, vars(p14_1ab_1:p14_1ab_15), personas)%>%
                                       "56 a 65 años",
                                       "Más de 65 años")),
          veinteomas = ifelse(edad>19, 1, 0), 
-         year=2016)
-
-data <- rename(data, si_trabaja=p14_1ab_1,
+         year=2016)%>%
+         mutate_at(vars(p11_6, p11_7, p13_4:p13_7_2), sino)%>%
+         mutate_at(vars(starts_with("p13_1_")), change)%>%
+         mutate(vio_fis=rowSums(.[,49:57], na.rm=T),
+                vio_psi=rowSums(.[,58:72], na.rm=T),
+                vio_sex=rowSums(.[,73:77], na.rm=T),
+                vio_eco=rowSums(.[,78:84], na.rm=T))%>%
+         mutate_at(vars(starts_with("vio")), suma)%>%
+         mutate_at(vars(starts_with("vio")), sino)%>%
+         select(-c(starts_with("p13_1_")))%>%
+         rename(si_trabaja=p14_1ab_1,
                      si_salir=p14_1ab_2,
                      quehacer_dinero=p14_1ab_3,
                      comprar_cosas=p14_1ab_4,
@@ -171,7 +198,12 @@ data <- rename(data, si_trabaja=p14_1ab_1,
                      ver_porno=p11_12_3,
                      int_vio=p11_12_4,
                      viola=p11_12_5,
-                     vio_din=p11_12_6)
+                     vio_din=p11_12_6,
+                     viofisinf=p11_6,
+                     viopsiinf=p11_7, 
+                     pidioayuda=p13_7_1,
+                     denuncio=p13_7_2,
+                     habloalg=p13_4)
 saveRDS(data, files$endireh16)
 
 
@@ -179,6 +211,7 @@ saveRDS(data, files$endireh16)
 files <- list(lib=paste0(direc, "endireh_2021/TB_SEC_XV.csv"),
               fam_ori=paste0(direc, "endireh_2021/TB_SEC_XII.csv"),
               socio=paste0(direc, "endireh_2021/TSDem.csv"),
+              pareja=paste0(direc, "endireh_2021/TB_SEC_XIV.csv"),
               endireh21=here("import-clean/output/endireh2021.rds"))
 
 
@@ -198,7 +231,16 @@ data <- left_join(data, tempo)
 tempo <- read.csv(files$fam_ori)%>%
          clean_names()%>%
          select(id_viv:t_instrum,
-                starts_with("p12_14_"))
+                starts_with("p12_14_"), p12_6, p12_7)
+
+data <- left_join(data, tempo)
+
+tempo <- read.csv(files$pareja)%>%
+         clean_names()%>%
+         select(id_viv:t_instrum,
+         starts_with("p14_1_"),
+         p14_4, p14_7_1, p14_7_2,
+         fac_viv:est_dis)
 
 data <- left_join(data, tempo)
 rm(tempo)
@@ -240,13 +282,23 @@ data <- mutate_at(data, vars(p15_1ab_1:p15_1ab_17), personas)%>%
                                       "56 a 65 años",
                                       "Más de 65 años")),
          veinteomas = ifelse(edad>19, 1, 0),
-         year=2021)
-
-data <- rename(data, si_trabaja=p15_1ab_1,
-               si_salir=p15_1ab_2,
-               quehacer_dinero=p15_1ab_3,
-               comprar_cosas=p15_1ab_4,
-               vida_social=p15_1ab_5,
+         p12_6=ifelse(p12_6<3, 1, 2),
+         p12_7=ifelse(p12_7<3, 1, 2),
+         year=2021)%>%
+         mutate_at(vars(p12_6, p12_7, p14_4:p14_7_2), sino)%>%
+         mutate_at(vars(starts_with("p14_1_")), change)%>%
+         mutate(vio_fis=rowSums(.[,47:55], na.rm=T),
+                vio_psi=rowSums(.[,56:70], na.rm=T),
+                vio_sex=rowSums(.[,71:77], na.rm=T),
+                vio_eco=rowSums(.[,78:84], na.rm=T))%>%
+  mutate_at(vars(starts_with("vio")), suma)%>%
+  mutate_at(vars(starts_with("vio")), sino)%>%
+  select(-c(starts_with("p14_1_")))%>%
+  rename(si_trabaja=p15_1ab_1,
+         si_salir=p15_1ab_2,
+         quehacer_dinero=p15_1ab_3,
+         comprar_cosas=p15_1ab_4,
+         vida_social=p15_1ab_5,
                vida_politica=p15_1ab_6,
                como_gasta=p15_1ab_7,
                dinero_el=p15_1ab_8,
@@ -264,7 +316,12 @@ data <- rename(data, si_trabaja=p15_1ab_1,
                ver_porno=p12_14_3,
                int_vio=p12_14_4,
                viola=p12_14_5,
-               vio_din=p12_14_6)
+               vio_din=p12_14_6,
+               viofisinf=p12_6,
+               viopsiinf=p12_7,
+               pidioayuda=p14_7_1,
+               denuncio=p14_7_2,
+               habloalg=p14_4)
 saveRDS(data, files$endireh21)
          
 # done.
